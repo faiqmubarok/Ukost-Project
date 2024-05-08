2bfaeda844a9380be9857bbb9d6271d45b25f2ed
MUHAMMAD FAIQ MUBAROK <faiq@MacBook-Air-MUHAMMAD.local>
2024-05-08 08:42:06 +0700

    commit last before backend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
         poppins: ["Poppins"],
       },
       colors: {
-        bluePrimary: '#1baaee',
-        blueSecond: '#55C2F5',
-        grayPrimary: '#757575',
-        graySecond: '#999999',
+        bluePrimary: "#1baaee",
+        blueSecond: "#55C2F5",
+        grayPrimary: "#757575",
+        graySecond: "#999999",
       },
       boxShadow: {
-        shadowFirst : "inset 0px 0px 20px rgba(0, 0, 0, .25)",
+        shadowFirst: "inset 0px 0px 20px rgba(0, 0, 0, .25)",
       },
     },
   },
   plugins: [],
-}
+};
